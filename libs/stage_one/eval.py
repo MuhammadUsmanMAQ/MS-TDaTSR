12c197c224ae046fa3b4d709fb5bd4b626f3e6e4
Muhammad Umer <thisismumer@gmail.com>
2022-06-22 15:10:43 +0500

    Added confidence score arguments to eval.py

--- libs/stage_one/eval.py
+++ libs/stage_one/eval.py
@@ -23,18 +23,18 @@ from tabulate import tabulate
     Evaluate Model
 """
 
-def evaluate_model_split(saved_model, data_dir):
+def evaluate_model_split(saved_model, data_dir, threshold):
     model = TDModel(use_pretrained_model = True, basemodel_requires_grad = True)
     model = model.to(config.device)
     model = model.float()   # Convert from float16 to float32
     last_epoch, tr_metrics, te_metrics = load_checkpoint(torch.load(saved_model, map_location = config.device), model)
 
     _, test_loader = get_data_loaders(data_dir)
-    metrics = test_on_epoch(test_loader, model, TDLoss(), threshold = 0.6)
+    metrics = test_on_epoch(test_loader, model, TDLoss(), threshold = threshold)
 
     return metrics
 
-def evaluate_model_test(saved_model, test_dir):
+def evaluate_model_test(saved_model, test_dir, threshold):
     model = TDModel(use_pretrained_model = True, basemodel_requires_grad = True)
     model = model.to(config.device)
     model = model.float()   # Convert from float16 to float32
@@ -44,7 +44,7 @@ def evaluate_model_test(saved_model, test_dir):
     test_dataset = Dataset(df, isTrain = False, transform = None)
     test_loader =  DataLoader(test_dataset, batch_size = config.batch_size, shuffle=False, num_workers = 4, pin_memory=True)
 
-    metrics = test_on_epoch(test_loader, model, TDLoss(), threshold = 0.6)
+    metrics = test_on_epoch(test_loader, model, TDLoss(), threshold = threshold)
 
     return metrics
 
@@ -60,11 +60,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Evaluation Metrics')
     parser.add_argument("--output_dir", help = "Dump metrics into a CSV file.", required = True)
     parser.add_argument("--model_dir", help = "Load pretrained model.", required = True)
-    parser.add_argument("--data_dir", help = "Path to locate.csv file.", required = False)
-    parser.add_argument("--test_dir", help = "Path to locate.csv file.", required = False)
-    parser.add_argument("--no_csv", type = bool, help = "Does not write the CSV file.", required = False)
+    parser.add_argument("--data_dir", help = "Path to complete data's locate.csv file. (Applies train/test split.)", required = False)
+    parser.add_argument("--test_dir", help = "Path to test data's locate.csv file. ", required = False)
+    parser.add_argument("--no_csv", type = bool, help = "Do not write the CSV file.", required = False)
+    parser.add_argument("--threshold", type = float, help = "Confidence score for calculating metrics. (Default = 0.6)", required = False)
     args = parser.parse_args()
     
+    if args.threshold is None:
+        threshold = 0.6
+    else:
+        threshold = args.threshold
+
     if args.data_dir is None and args.test_dir is None:
         parser.error("At least one of --data_dir and --test_dir is required.")
 
@@ -79,9 +85,9 @@ if __name__ == '__main__':
     newdir = tempdir[:size - 8]
 
     if args.data_dir is not None:
-        data = evaluate_model_split(saved_model, data_dir)
+        data = evaluate_model_split(saved_model, data_dir, threshold)
     else:
-        data = evaluate_model_test(saved_model, test_dir)
+        data = evaluate_model_test(saved_model, test_dir, threshold)
 
     r_data = dict()
 
