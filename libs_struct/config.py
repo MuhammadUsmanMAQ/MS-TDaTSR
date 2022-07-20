4a85f39e51f00337a9b283acfb666ac94d5035c2
Muhammad Umer <thisismumer@gmail.com>
2022-07-20 20:09:56 +0500

    Added structure recognition libraries.

---
+++ libs_struct/config.py
@@ -0,0 +1,305 @@
+base_dir = "TO_BE_CONFIGURED"
+dataset_type = "CocoDataset"
+data_root = "TO_BE_CONFIGURED"
+img_norm_cfg = dict(mean=[127.5, 127.5, 127.5], std=[127.5, 127.5, 127.5], to_rgb=True)
+train_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations", with_bbox=True),
+    dict(
+        type="Resize",
+        img_scale=[(1333, 768), (1024, 512), (896, 896), (768, 1333), (512, 1024)],
+        multiscale_mode="range",
+        keep_ratio=True,
+    ),
+    dict(
+        type="Normalize",
+        mean=[127.5, 127.5, 127.5],
+        std=[127.5, 127.5, 127.5],
+        to_rgb=True,
+    ),
+    dict(type="Pad", size_divisor=32),
+    dict(type="DefaultFormatBundle"),
+    dict(type="Collect", keys=["img", "gt_bboxes", "gt_labels"]),
+]
+test_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(
+        type="MultiScaleFlipAug",
+        img_scale=(896, 896),
+        flip=False,
+        transforms=[
+            dict(type="Resize", keep_ratio=True),
+            dict(type="RandomFlip"),
+            dict(
+                type="Normalize",
+                mean=[127.5, 127.5, 127.5],
+                std=[127.5, 127.5, 127.5],
+                to_rgb=True,
+            ),
+            dict(type="Pad", size_divisor=32),
+            dict(type="DefaultFormatBundle"),
+            dict(type="Collect", keys=["img"]),
+        ],
+    ),
+]
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=2,
+    train=dict(
+        type="CocoDataset",
+        ann_file="TO_BE_CONFIGURED/train/_annotations.coco.json",
+        img_prefix="TO_BE_CONFIGURED/train/",
+        pipeline=[
+            dict(type="LoadImageFromFile"),
+            dict(type="LoadAnnotations", with_bbox=True),
+            dict(type="Resize", img_scale=(1333, 800), keep_ratio=True),
+            dict(type="RandomFlip", flip_ratio=0.5),
+            dict(
+                type="Normalize",
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True,
+            ),
+            dict(type="Pad", size_divisor=32),
+            dict(type="DefaultFormatBundle"),
+            dict(type="Collect", keys=["img", "gt_bboxes", "gt_labels"]),
+        ],
+        classes=("cell",),
+    ),
+    val=dict(
+        type="CocoDataset",
+        ann_file="TO_BE_CONFIGURED/valid/_annotations.coco.json",
+        img_prefix="TO_BE_CONFIGURED/valid/",
+        pipeline=[
+            dict(type="LoadImageFromFile"),
+            dict(
+                type="MultiScaleFlipAug",
+                img_scale=(1333, 800),
+                flip=False,
+                transforms=[
+                    dict(type="Resize", keep_ratio=True),
+                    dict(type="RandomFlip"),
+                    dict(
+                        type="Normalize",
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True,
+                    ),
+                    dict(type="Pad", size_divisor=32),
+                    dict(type="ImageToTensor", keys=["img"]),
+                    dict(type="Collect", keys=["img"]),
+                ],
+            ),
+        ],
+        classes=("cell",),
+    ),
+    test=dict(
+        type="CocoDataset",
+        ann_file="TO_BE_CONFIGURED/valid/_annotations.coco.json",
+        img_prefix="TO_BE_CONFIGURED/valid/",
+        pipeline=[
+            dict(type="LoadImageFromFile"),
+            dict(
+                type="MultiScaleFlipAug",
+                img_scale=(1333, 800),
+                flip=False,
+                transforms=[
+                    dict(type="Resize", keep_ratio=True),
+                    dict(type="RandomFlip"),
+                    dict(
+                        type="Normalize",
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True,
+                    ),
+                    dict(type="Pad", size_divisor=32),
+                    dict(type="ImageToTensor", keys=["img"]),
+                    dict(type="Collect", keys=["img"]),
+                ],
+            ),
+        ],
+        classes=("cell",),
+    ),
+)
+evaluation = dict(interval=3, metric="bbox")
+checkpoint_config = dict(interval=3)
+log_config = dict(
+    interval=50, hooks=[dict(type="TextLoggerHook"), dict(type="TensorboardLoggerHook")]
+)
+custom_hooks = [dict(type="NumClassCheckHook")]
+dist_params = dict(backend="nccl")
+log_level = "INFO"
+load_from = None
+resume_from = None
+workflow = [("train", 1)]
+opencv_num_threads = 0
+mp_start_method = "fork"
+auto_scale_lr = dict(enable=False, base_batch_size=4)
+model = dict(
+    type="GridRCNN",
+    backbone=dict(
+        type="ResNet",
+        depth=101,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type="BN", requires_grad=True),
+        norm_eval=True,
+        style="pytorch",
+        init_cfg=None,
+    ),
+    neck=dict(
+        type="FPN", in_channels=[256, 512, 1024, 2048], out_channels=256, num_outs=5
+    ),
+    rpn_head=dict(
+        type="RPNHead",
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type="AnchorGenerator",
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64],
+        ),
+        bbox_coder=dict(
+            type="DeltaXYWHBBoxCoder",
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[1.0, 1.0, 1.0, 1.0],
+        ),
+        loss_cls=dict(type="CrossEntropyLoss", use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type="SmoothL1Loss", beta=0.1111111111111111, loss_weight=1.0),
+    ),
+    roi_head=dict(
+        type="GridRoIHead",
+        bbox_roi_extractor=dict(
+            type="SingleRoIExtractor",
+            roi_layer=dict(type="RoIAlign", output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32],
+        ),
+        bbox_head=dict(
+            type="Shared2FCBBoxHead",
+            with_reg=False,
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=1,
+            bbox_coder=dict(
+                type="DeltaXYWHBBoxCoder",
+                target_means=[0.0, 0.0, 0.0, 0.0],
+                target_stds=[0.1, 0.1, 0.2, 0.2],
+            ),
+            reg_class_agnostic=False,
+        ),
+        grid_roi_extractor=dict(
+            type="SingleRoIExtractor",
+            roi_layer=dict(type="RoIAlign", output_size=14, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32],
+        ),
+        grid_head=dict(
+            type="GridHead",
+            grid_points=9,
+            num_convs=8,
+            in_channels=256,
+            point_feat_channels=64,
+            norm_cfg=dict(type="GN", num_groups=36),
+            loss_grid=dict(type="CrossEntropyLoss", use_sigmoid=True, loss_weight=15),
+        ),
+        train_cfg=dict(
+            assigner=dict(
+                type="MaxIoUAssigner",
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                ignore_iof_thr=-1,
+            ),
+            sampler=dict(
+                type="RandomSampler",
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True,
+            ),
+            pos_radius=1,
+            pos_weight=-1,
+            max_num_grid=192,
+            debug=False,
+        ),
+        test_cfg=dict(
+            score_thr=0.03, nms=dict(type="nms", iou_threshold=0.3), max_per_img=100
+        ),
+        pretrained=None,
+    ),
+    train_cfg=dict(
+        rpn=dict(
+            assigner=dict(
+                type="MaxIoUAssigner",
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                ignore_iof_thr=-1,
+            ),
+            sampler=dict(
+                type="RandomSampler",
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False,
+            ),
+            allowed_border=0,
+            pos_weight=-1,
+            debug=False,
+        ),
+        rpn_proposal=dict(
+            nms_pre=2000,
+            max_per_img=2000,
+            nms=dict(type="nms", iou_threshold=0.7),
+            min_bbox_size=0,
+        ),
+        rcnn=dict(
+            assigner=dict(
+                type="MaxIoUAssigner",
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                ignore_iof_thr=-1,
+            ),
+            sampler=dict(
+                type="RandomSampler",
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True,
+            ),
+            pos_radius=1,
+            pos_weight=-1,
+            max_num_grid=192,
+            debug=False,
+        ),
+    ),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=1000,
+            max_per_img=1000,
+            nms=dict(type="nms", iou_threshold=0.7),
+            min_bbox_size=0,
+        ),
+        rcnn=dict(
+            score_thr=0.03, nms=dict(type="nms", iou_threshold=0.3), max_per_img=100
+        ),
+    ),
+)
+optimizer = dict(type="SGD", lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+lr_config = dict(
+    policy="step", warmup="linear", warmup_iters=280, warmup_ratio=0.001, step=[21, 28]
+)
+runner = dict(type="EpochBasedRunner", max_epochs=30)
+custom_imports = dict(imports=["mmcls.models"], allow_failed_imports=False)
+crop_size = None
+classes = ("cell",)
+work_dir = "TO_BE_CONFIGURED"
+device = "cuda"
+seed = 0
+gpu_ids = range(0, 1)
---
+++ libs_struct/inference.py
@@ -0,0 +1,177 @@
+# Common Libraries
+import os
+import os.path as osp
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import torch, torchvision
+
+# OpenMMLabs
+import mmdet
+from mmcv.runner import load_checkpoint
+from mmdet.apis import inference_detector, show_result_pyplot, set_random_seed
+from mmdet.models import build_detector
+from mmdet.apis.inference import init_detector
+import mmcv
+
+import subprocess
+import argparse
+from config import base_dir
+from termcolor import colored
+import warnings
+
+warnings.simplefilter("ignore", UserWarning)
+
+"""
+    Inference Model
+"""
+
+
+def solve(R1, R2):
+    if (R1[0] >= R2[2]) or (R1[2] <= R2[0]) or (R1[3] <= R2[1]) or (R1[1] >= R2[3]):
+        return False
+    else:
+        return True
+
+
+def getArea(box):
+    return (box[2] - box[0]) * (box[3] - box[1])
+
+
+def upscale_image(upscale_model, input_dir, device):
+    image = cv2.imread(str(input_dir), cv2.IMREAD_COLOR)
+    path = os.path.split(str(input_dir))[0]
+    base_name = os.path.split(str(input_dir))[1]
+
+    os.makedirs("input/", exist_ok=True)
+    os.makedirs("output/", exist_ok=True)
+
+    cv2.imwrite(
+        f"input/{base_name}", image[:, :, :3]
+    )  # Removing possible alpha channel
+
+    if device == "cpu":
+        proc = subprocess.run(f"python upscale.py -m {upscale_model} -cpu", shell=True,)
+    else:
+        proc = subprocess.run(f"python upscale.py -m {upscale_model}", shell=True,)
+
+    return proc.returncode
+
+
+def detect_structure(image, config_file, checkpoint_file, device, thresh):
+    model = init_detector(config_file, checkpoint_file, device=device)
+    result = inference_detector(model, image)  # Prints array of bbox
+
+    det_boxes = []
+    for r in result[0]:
+        if r[4] > thresh:
+            det_boxes.append(r.astype(int))
+
+    det_boxes = np.array(det_boxes)[
+        :, :4
+    ]  # Each element is of format [xmin, ymin, xmax, ymax]
+    det_boxes = det_boxes.tolist()
+    final_boxes = det_boxes.copy()
+
+    for i in range(len(det_boxes)):
+        for k in range(len(det_boxes)):
+            if (k != i) and (solve(det_boxes[i], det_boxes[k]) == True):
+                if (det_boxes[i] in final_boxes) and (
+                    getArea(det_boxes[i]) < getArea(det_boxes[k])
+                ):
+                    final_boxes.remove(det_boxes[i])
+            else:
+                pass
+
+    return final_boxes
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Perform Table Detection > Table Structure Extraction"
+    )
+    parser.add_argument("--input_img", help="Path to input image.", required=True)
+    parser.add_argument(
+        "--upscale",
+        help="Upscale / Yields better results in most scenarios.",
+        dest="upscale",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--no-upscale", help="Disable upscaling.", dest="upscale", action="store_false"
+    )
+    parser.set_defaults(upscale=True)
+    parser.add_argument(
+        "--upscale_model",
+        help="Load image upscaling model, Options: '4x-AnimeSharp', 'NKMDTypeScale'. Read: http://upscale.wiki/",
+        default="4x-AnimeSharp",
+        required=False,
+    )
+    parser.add_argument(
+        "--struct_weights",
+        help="Load structure recognition model.",
+        default=osp.join(f"{base_dir}", "models/struct/grid_rcnn/best_ckpt.pth"),
+        required=False,
+    )
+    args = parser.parse_args()
+    base_name = os.path.split(str(args.input_img))[1]
+
+    device = str(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    if device == "cpu":
+        print(colored("Using CPU runtime, this may take a while.", "red"))
+        print(
+            colored(
+                "Not what is expected? -> Ensure that CUDA-Ready version of PyTorch is installed.",
+                "red",
+            )
+        )
+    else:
+        print(
+            colored(
+                "Using CUDA runtime to perform upscaling & structure recognition.",
+                "red",
+            )
+        )
+    if args.upscale:
+        stat = upscale_image(args.upscale_model, args.input_img, device)
+
+        if stat == 0:
+            print(colored("Upscaled input successfully.", "green"))
+            ori_img = cv2.imread(osp.abspath(f"output/{base_name}"), cv2.IMREAD_COLOR)
+            ori_img = ori_img[:, :, :3]
+        elif stat == 1:
+            print(colored("Recheck argument directiories and try again.", "red"))
+            exit()
+        else:
+            print(
+                colored(
+                    "Upscale did not complete successfully. Proceeding without upscaling.",
+                    "red",
+                )
+            )
+            ori_img = cv2.imread(str(args.input_img), cv2.IMREAD_COLOR)
+            ori_img = ori_img[:, :, :3]  # Removing possible alpha channel
+
+    else:
+        print(colored("Upscale set to False. Proceeding without upscaling.", "green"))
+        ori_img = cv2.imread(str(args.input_img), cv2.IMREAD_COLOR)
+        ori_img = ori_img[:, :, :3]  # Removing possible alpha channel
+
+    struct_img = ori_img.copy()
+    checkpoint_file = args.struct_weights
+    print(checkpoint_file)
+    struct_boxes = detect_structure(
+        struct_img, "config.py", checkpoint_file, device, 0.3
+    )
+
+    for i in range(len(struct_boxes)):
+        struct_img = cv2.rectangle(
+            struct_img,
+            (struct_boxes[i][0], struct_boxes[i][1]),
+            (struct_boxes[i][2], struct_boxes[i][3]),
+            (0, 255, 0),
+            2,
+        )
+
+    cv2.imwrite(f"results/{base_name}", struct_img)
